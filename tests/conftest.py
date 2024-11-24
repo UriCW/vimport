18bdbbfe1e3a14811dc75832cfe0b94c0aef545f
UriCW <Uri.Cohen.Waisel@Gmail.com>
2024-11-24 14:52:53 +0100

    fixed a test

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,7 +7,7 @@ os.environ["ENVIRONMENT"] = "test"
 
 @pytest.fixture
 def app():
-    app = create_app("test")
+    app = create_app()
     with app.app_context():
         db.create_all()
         yield app
